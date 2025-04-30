1d3d1d87f480ee774189b092292eb057486934d2
Love2502 <luvsharma9707@gmail.com>
2025-04-30 14:42:22 +0200

    Create data.py
    https://archive.ics.uci.edu/dataset/198/steel+plates+faults

---
+++ data.py
@@ -0,0 +1,15 @@
+pip install ucimlrepo
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+steel_plates_faults = fetch_ucirepo(id=198) 
+  
+# data (as pandas dataframes) 
+X = steel_plates_faults.data.features 
+y = steel_plates_faults.data.targets 
+  
+# metadata 
+print(steel_plates_faults.metadata) 
+  
+# variable information 
+print(steel_plates_faults.variables) 
